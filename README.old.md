bc8377d41ec5f2ba5fcc7c4626413a684cb7514c
manishkumar <manishkumar13may2003@gmail.com>
2024-02-17 01:43:59 +0530

    initial commit

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# Blog_App
--Command to import React to your directory ( npx create-react-app ./)
---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,17 @@
+import "./topbar.css"
+
+export default function TopBar() {
+  return (
+    <div className="top">
+      <div className="topLeft">
+
+      <i class="fa-brands fa-facebook"></i>
+      <i class="fa-brands fa-x-twitter"></i>
+      <i class="fa-brands fa-instagram"></i>
+      </div>
+      <div className="topCentre"></div>
+      <div className="topRight"></div>
+
+    </div>
+  )
+}
